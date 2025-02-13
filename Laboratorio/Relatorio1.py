c49c3d67e3d97cbc4be34dd7f49a8ef60ca63eb0
LucasLimaGadbem <lucaslimagadbem@gmail.com>
2025-02-13 15:32:44 -0300

    inserindo relatorio 1 do laboratorio

---
+++ Laboratorio/Relatorio1.py
@@ -0,0 +1,39 @@
+class Professor:
+    def __init__(self, nome):
+        self.nome = nome
+
+    def __str__(self):
+        return self.nome
+
+    def ministrar_aula(self, assunto):
+        print(f'O professor {self.nome} esta ministrando uma aula sobre {assunto}')
+
+class Aluno:
+    def __init__(self, nome):
+        self.nome = nome
+
+    def presenca(self):
+        print(f'O aluno {self.nome} esta presente')
+
+class Aula:
+    def __init__(self, professor, assunto, alunos):
+        self.professor = professor
+        self.assunto = assunto
+        self.alunos = alunos if isinstance(alunos,list) else [alunos]
+
+    def adicionar_aluno(self, alunos):
+        self.alunos.append(alunos)
+
+    def listar_presenca(self):
+        print(f'Presenca na aula sobre {self.assunto}, '
+              f'ministrada pelo professor {self.professor}: ')
+        for aluno in self.alunos:
+            aluno.presenca()
+
+
+professor = Professor("Lucas")
+aluno1 = Aluno("Maria")
+aluno2 = Aluno("Pedro")
+aula = Aula(professor, "Programacao Orientada a Objetos", aluno1)
+aula.adicionar_aluno(aluno2)
+aula.listar_presenca()
